7421bce2b9416363061752c62138ba169a919202
Bruno Braat <bruno.braat@gmail.com>
2023-08-07 15:37:48 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const pup = require('pupeteer');
+const pup = require('puppeteer');
 
 const url = "https://www.mercadolivre.com.br/";
 const searchFor = "macbook";
 
 (async () => {
     //inicializa o navegador
-    const browser = await pup.launch();
+    const browser = await pup.launch({ headless: false });
     const page = await browser.newPage();
     console.log('Iniciei');
 
     await page.goto(url);
     console.log('Fui para a url!')
+
+    await page.waitForSelector('#cb1-edit');
+
+    await page.type('#cb1-edit', searchFor);
+
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('.nav-search-btn')
+    ])
+
+    const links = await page.$$eval('ui-search-result-image__element > a', el => el.map(link => link.href));
+    console.log(links);
+
+    await page.setTimeout(() => {
+        
+    }, 3000);
 
     await browser.close();
 })();
